adb3afbf19471d82ffa92673b93cfab8af8f2a8a
Gautam-00 <gautamatuag000@gmail.com>
2025-11-23 01:26:52 +0530

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = process.env.PORT || 5555;
+
+const atlasURL = "mongodb+srv://gautamatuag540_db_user:a7KRA6gmYowCiD2z@cluster0.cnlk4ps.mongodb.net/?appName=Cluster0"
+export const mongoDBURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/books-collection';
+
